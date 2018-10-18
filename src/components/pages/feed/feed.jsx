10c62b1ae356c12bcae65d1962ca4135958ec539
Slava Ermakov <slavaskainet@gmail.com>
2018-10-18 19:57:34 +0300

    Check working git

--- src/components/pages/feed/feed.jsx
+++ src/components/pages/feed/feed.jsx
@@ -8,7 +8,7 @@ export default class Feed extends Component {
     render() {
         return (
             <div className="feed">
-                <p className="feed-label">Future feed of photo</p>
+                <p className="feed-label">Future feed of photos</p>
             </div>
         );
     }
