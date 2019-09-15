f2b3815a9855d6d8f0cdba3c6c638d5bb7d035a3
Vyacheslav Ermakov <vyacheslav.ermakov@onetwotrip.com>
2019-09-15 14:23:58 +0300

    deploy

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const compression = require("compression");
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(compression());
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+app.listen(port);
